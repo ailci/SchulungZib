0a92ff2a6fbb992d7159c9f5f48f2d0791337c94
Ali Ilci <ali.ilci@gmx.de>
2026-03-03 14:39:24 +0100

    IAuthorService

---
+++ Application/Contracts/Services/IAuthorService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Application.ViewModels.Author;
+
+namespace Application.Contracts.Services;
+
+public interface IAuthorService
+{
+    Task<IEnumerable<AuthorViewModel>> GetAuthorsAsync();
+}
