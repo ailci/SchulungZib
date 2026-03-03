ded02dfcc772ac89a86fe28ea2df0576474a2c77
Ali Ilci <ali.ilci@gmx.de>
2026-03-03 14:31:59 +0100

    Util

---
+++ Application/Utilities/Util.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
+
+namespace Application.Utilities;
+
+public static class Util
+{
+    public static string LogAsJson<T>(this T obj) where T : class
+    {
+        return JsonSerializer.Serialize(obj, new JsonSerializerOptions { WriteIndented = true });
+    }
+}
--- UI.Blazor/Components/Pages/Home.razor.cs
+++ UI.Blazor/Components/Pages/Home.razor.cs
@@ -1,4 +1,5 @@
 using Application.Contracts.Services;
+using Application.Utilities;
 using Application.ViewModels.Qotd;
 using Infrastructure;
 using Microsoft.AspNetCore.Components;
@@ -7,11 +8,17 @@ namespace UI.Blazor.Components.Pages;
 
 public partial class Home
 {
-    public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
+    [Inject] public ILogger<Home> Logger { get; set; } = null!;
     [Inject] public IServiceManager ServiceManager { get; set; } = null!;
+
+    public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
 
     protected override async Task OnInitializedAsync()
     {
+        Logger.LogInformation("Home aufgerufen...");
+
         QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+
+        Logger.LogInformation($"QotdViewModel => {QotdViewModel?.LogAsJson()}");
     }
 }
