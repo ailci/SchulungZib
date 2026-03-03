b22a4107ecffad161ed56d222706805d9067a3ef
Ali Ilci <ali.ilci@gmx.de>
2026-03-03 15:29:54 +0100

    Authors

--- UI.Blazor/Components/Pages/Author/Overview.razor.cs
+++ UI.Blazor/Components/Pages/Author/Overview.razor.cs
@@ -1,8 +1,17 @@
-using System.Collections;
+using Application.Contracts.Services;
 using Application.ViewModels.Author;
+using Microsoft.AspNetCore.Components;
+using System.Collections;
 
 namespace UI.Blazor.Components.Pages.Author;
 public partial class Overview
 {
+    [Inject] public ILogger<Overview> Logger { get; set; } = null!;
+    [Inject] public IServiceManager ServiceManager { get; set; } = null!;
     public IEnumerable<AuthorViewModel>? AuthorsVm { get; set; }
+
+    protected override async Task OnInitializedAsync()
+    {
+        AuthorsVm = await ServiceManager.AuthorService.GetAuthorsAsync();
+    }
 }
