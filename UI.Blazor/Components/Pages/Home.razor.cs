c94b3ae5006d7aceeb86531f16965b4262a5cf7a
Ali Ilci <ali.ilci@gmx.de>
2026-03-03 16:28:33 +0100

    PeristData

--- UI.Blazor/Components/Pages/Home.razor.cs
+++ UI.Blazor/Components/Pages/Home.razor.cs
@@ -6,19 +6,49 @@ using Microsoft.AspNetCore.Components;
 
 namespace UI.Blazor.Components.Pages;
 
-public partial class Home
+public partial class Home : IDisposable
 {
     [Inject] public ILogger<Home> Logger { get; set; } = null!;
     [Inject] public IServiceManager ServiceManager { get; set; } = null!;
-
+    [Inject] public PersistentComponentState ApplicationState { get; set; } = null!;
+    private PersistingComponentStateSubscription _persistingComponentStateSubscription;
     public QuoteOfTheDayViewModel? QotdViewModel { get; set; }
+    
 
     protected override async Task OnInitializedAsync()
     {
         Logger.LogInformation("Home aufgerufen...");
 
-        QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+        //3.Lösung
+        _persistingComponentStateSubscription = ApplicationState.RegisterOnPersisting(PersistData);
+        if (!ApplicationState.TryTakeFromJson<QuoteOfTheDayViewModel>(nameof(QotdViewModel), out var restoredData))
+        {
+            //Im PersistState nicht vorhanden
+            QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+        }
+        else
+        {
+            QotdViewModel = restoredData;
+        }
 
-        Logger.LogInformation($"QotdViewModel => {QotdViewModel?.LogAsJson()}");
+        //Logger.LogInformation($"QotdViewModel => {QotdViewModel?.LogAsJson()}");
     }
+
+    private Task PersistData()
+    {
+        ApplicationState.PersistAsJson(nameof(QotdViewModel), QotdViewModel);
+        return Task.CompletedTask;
+    }
+
+    public void Dispose() => _persistingComponentStateSubscription.Dispose();
+
+    // 2. Lösung
+    //protected override async Task OnAfterRenderAsync(bool firstRender)
+    //{
+    //    if (firstRender)
+    //    {
+    //        QotdViewModel = await ServiceManager.QotdService.GetQuoteOfTheDayAsync();
+    //        StateHasChanged();
+    //    }
+    //}
 }
