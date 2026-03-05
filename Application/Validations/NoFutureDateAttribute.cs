ff8d3b76837bbaa7505d61d3e81f56fe24e5e43e
Ali Ilci <ali.ilci@gmx.de>
2026-03-05 11:55:06 +0100

    ValidationAttribute

---
+++ Application/Validations/NoFutureDateAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Application.ViewModels.Author;
+
+namespace Application.Validations;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class NoFutureDateAttribute : ValidationAttribute
+{
+    public NoFutureDateAttribute()
+    {
+        if (string.IsNullOrEmpty(ErrorMessage)) ErrorMessage = "Datum liegt in der Zukunft";
+    }
+
+    protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+    {
+        //var authorForCreateViewModel = (AuthorForCreateViewModel) validationContext.ObjectInstance;
+        if (value is DateOnly dateToCheck)
+        {
+            if (dateToCheck > DateOnly.FromDateTime(DateTime.Today))
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+        }
+
+        return ValidationResult.Success;
+    }
+}
--- Application/ViewModels/Author/AuthorForCreateViewModel.cs
+++ Application/ViewModels/Author/AuthorForCreateViewModel.cs
@@ -1,15 +1,26 @@
 ﻿using Microsoft.AspNetCore.Components.Forms;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
+using Application.Validations;
 
 namespace Application.ViewModels.Author;
 
 public class AuthorForCreateViewModel
 {
     public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "Bitte geben Sie ienen Namen ein")]
+    [Length(2, 150, ErrorMessage = "Name ist zu kurz/lang")]
+    [DeniedValues(["administrator","admin","root","god"], ErrorMessage = "Der Name ist nicht erlaubt")]
     public required string Name { get; set; }
+
+    [Required(ErrorMessage = "Bitte geben Sie ienen Namen ein")]
+    [MinLength(2, ErrorMessage = "Name ist zu kurz")]
     public required string Description { get; set; }
+
+    [NoFutureDate(ErrorMessage = "Geburtsdatum liegt in der Zukunft")]
     public DateOnly? BirthDate { get; set; }
     public IBrowserFile? Photo { get; set; }
 }
